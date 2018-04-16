804241ea8bcd862702febde9f64eff13a53d8c7b
Alirio Rivera <alirio.riveracuervo@salesforce.com>
2018-04-16 12:27:40 -0400

    other

--- example_go_test.go
+++ example_go_test.go
@@ -6,13 +6,13 @@ import (
 )
 
 //commands
-var _ = Describe("Example", func() {
-	Describe("Make an awesome expression", func() {
-		Context("smile", func() {
-			It("should result :)", func() {
-				Setup()
-				Expect(GetResult()).To(Equal(":)"))
-			})
-		})
-	})
-})
+// var _ = Describe("Example", func() {
+// 	Describe("Make an awesome expression", func() {
+// 		Context("smile", func() {
+// 			It("should result :)", func() {
+// 				Setup()
+// 				Expect(GetResult()).To(Equal(":)"))
+// 			})
+// 		})
+// 	})
+// })
